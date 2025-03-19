884c08fd90efc0045bc7a88622870967f91a5108
Aman Patidar <amanpatidar514@gmail.com>
2025-03-20 00:54:39 +0530

    Updated CSP and callback handling

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -5,31 +5,36 @@ const Callback = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const hash = window.location.hash;
-
-    if (hash) {
-      const params = new URLSearchParams(hash.substring(1));
-      const accessToken = params.get('access_token');
-
-      if (accessToken) {
-        // Store the token in localStorage
-        localStorage.setItem('spotify_access_token', accessToken);
-
-        // Clean up the URL by removing the hash
-        window.history.replaceState({}, document.title, window.location.pathname);
-
-        // Redirect to rappers page
-        navigate('/rappers');
-      } else {
-        // If no access token is found, navigate to home or show error
-        navigate('/');
+    const getHashParams = () => {
+      const hashParams = {};
+      const r = /([^&;=]+)=?([^&;]*)/g;
+      const q = window.location.hash.substring(1);
+      let e;
+      while (e = r.exec(q)) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
       }
+      return hashParams;
+    };
+
+    const params = getHashParams();
+    if (params.access_token) {
+      localStorage.setItem('spotify_access_token', params.access_token);
+      navigate('/rappers');
+    } else {
+      navigate('/');
     }
   }, [navigate]);
 
   return (
-    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background: '#000' }}>
-      <h2 style={{ color: '#fff' }}>Loading...</h2>
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#000',
+      color: '#fff'
+    }}>
+      <h2>Connecting to Spotify...</h2>
     </div>
   );
 };
