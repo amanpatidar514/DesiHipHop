53172df4ed5526dbe8c5d01c37140b4970eb942e
Aman Patidar <amanpatidar514@gmail.com>
2025-04-01 20:52:20 +0530

    Spotify login removed

--- src/pages/Rappers.js
+++ src/pages/Rappers.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Rappers.css';
 
 const SPOTIFY_CLIENT_ID = '7c51bc90b0884fa5afc2d1420b995a61';
